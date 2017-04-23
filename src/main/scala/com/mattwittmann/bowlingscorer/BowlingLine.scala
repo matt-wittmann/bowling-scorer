1151cbd93ef798bab07c0fe1235b8a82e85765c8
Matt Wittmann <matt@mattwittmann.com>
2017-04-23 13:09:07 -0500

    Some refactoring: extracting private helper methods

--- src/main/scala/com/mattwittmann/bowlingscorer/BowlingLine.scala
+++ src/main/scala/com/mattwittmann/bowlingscorer/BowlingLine.scala
@@ -17,52 +17,69 @@ object BowlingLine {
   private[this] val NoBonuses = (Nil, Nil)
   case class State(currentFrame: Int = 0, roll: Roll = FirstRoll, bonuses: Bonuses = NoBonuses, frames: BowlingLine = UnplayedFrames)
 
+  private[this] def calculatePinsKnockedDownAndCurrentFrameScore(lastState: State, roll: Char): (Int, Score) = {
+    val lastCurrentFrameScore = lastState.frames(lastState.currentFrame)
+
+    roll match {
+      case 'X' => (10, lastCurrentFrameScore + 10)
+      case '/' => (10 - lastCurrentFrameScore, 10)
+      case '-' => (0, lastCurrentFrameScore)
+      case digit => {
+        val convertedDigit = digit.asDigit
+        (convertedDigit, lastCurrentFrameScore + convertedDigit)
+      }
+    }
+  }
+
+  private[this] def calculateNextFrameAndRoll(lastState: State, roll: Char): (Int, Roll) = {
+    val currentFrame = lastState.currentFrame
+    val lastRoll = lastState.roll
+
+    if ('X' == roll && currentFrame == LastFrame && lastRoll == FirstRoll)
+      (LastFrame, FirstBonusRoll)
+    else if (lastRoll == FirstBonusRoll || ('/' == roll && currentFrame == LastFrame && lastRoll == SecondRoll))
+      (LastFrame, LastBonusRoll)
+    else if (lastRoll == LastBonusRoll)
+      (LastFrame, Over)
+    else if (lastRoll == SecondRoll && currentFrame == LastFrame)
+      (LastFrame, Over)
+    else if (lastRoll == SecondRoll || 'X' == roll)
+      (currentFrame + 1, FirstRoll)
+    else
+      (currentFrame, SecondRoll)
+  }
+
+  private[this] def calculateNextBonuses(lastState: State, roll: Char): Bonuses = {
+    val currentFrame = lastState.currentFrame
+
+    if (roll == 'X')
+      (currentFrame :: lastState.bonuses._2, List(currentFrame))
+    else if (roll == '/')
+      (currentFrame :: lastState.bonuses._2, Nil)
+    else
+      (lastState.bonuses._2, Nil)
+  }
+
+  private[this] def calculateNextFrames(lastState: State, roll: Char): BowlingLine = {
+    val (pinsKnockedDown, currentFrameScore) = calculatePinsKnockedDownAndCurrentFrameScore(lastState, roll)
+
+    lastState.frames.zipWithIndex.map {
+      case (score, index) => {
+        if (index == lastState.currentFrame)
+          currentFrameScore
+        else if (lastState.bonuses._1.contains(index))
+          score + pinsKnockedDown
+        else
+          score
+      }
+    }
+  }
+
   def score(line: String): Score = {
     line.foldLeft[State](State()) { (lastState, roll) =>
-      val currentFrame = lastState.currentFrame
-      val lastCurrentFrameScore = lastState.frames(currentFrame)
-      val (pinsKnockedDown, currentFrameScore) = roll match {
-        case 'X' => (10, lastCurrentFrameScore + 10)
-        case '/' => (10 - lastCurrentFrameScore, 10)
-        case '-' => (0, lastCurrentFrameScore)
-        case digit => {
-          val convertedDigit = digit.asDigit
-          (convertedDigit, lastCurrentFrameScore + convertedDigit)
-        }
-      }
-
-      val (nextFrame, nextRoll) =
-        if ('X' == roll && currentFrame == LastFrame && lastState.roll == FirstRoll)
-          (LastFrame, FirstBonusRoll)
-        else if (lastState.roll == FirstBonusRoll || ('/' == roll && currentFrame == LastFrame && lastState.roll == SecondRoll))
-          (LastFrame, LastBonusRoll)
-        else if (lastState.roll == LastBonusRoll)
-          (LastFrame, Over)
-        else if (lastState.roll == SecondRoll && currentFrame == LastFrame)
-          (LastFrame, Over)
-        else if (lastState.roll == SecondRoll || 'X' == roll)
-          (currentFrame + 1, FirstRoll)
-        else
-          (currentFrame, SecondRoll)
-
-      val nextBonuses =
-        if (roll == 'X')
-          (currentFrame :: lastState.bonuses._2, List(currentFrame))
-        else if (roll == '/')
-          (currentFrame :: lastState.bonuses._2, Nil)
-        else
-          (lastState.bonuses._2, Nil)
-
-      val nextFrames: BowlingLine = lastState.frames.zipWithIndex.map {
-        case (score, index) => {
-          if (index == currentFrame)
-            currentFrameScore
-          else if (lastState.bonuses._1.contains(index))
-            score + pinsKnockedDown
-          else
-            score
-        }
-      }
+      val (nextFrame, nextRoll) = calculateNextFrameAndRoll(lastState, roll)
+      val nextBonuses = calculateNextBonuses(lastState, roll)
+      val nextFrames = calculateNextFrames(lastState, roll)
 
       State(nextFrame, nextRoll, nextBonuses, nextFrames)
     }.frames.sum
