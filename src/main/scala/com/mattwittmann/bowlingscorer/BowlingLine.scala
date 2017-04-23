69f3ed9cb45bbbc7433de30e425d0426eb151273
Matt Wittmann <matt@mattwittmann.com>
2017-04-23 10:06:56 -0500

    Handling test case with score of 80

--- src/main/scala/com/mattwittmann/bowlingscorer/BowlingLine.scala
+++ src/main/scala/com/mattwittmann/bowlingscorer/BowlingLine.scala
@@ -1,5 +1,8 @@
 package com.mattwittmann.bowlingscorer
 
 object BowlingLine {
-  def score(line: String): Int = 0
+  def score(line: String): Int = line.map {
+    case '-' => 0
+    case roll => roll.asDigit
+  }.sum
 }
