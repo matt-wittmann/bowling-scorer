bb00ac2b6722421db45ba1aa847668d310fc3ae0
Matt Wittmann <matt@mattwittmann.com>
2017-04-23 12:50:06 -0500

    Passing all unit tests

--- src/main/scala/com/mattwittmann/bowlingscorer/BowlingLine.scala
+++ src/main/scala/com/mattwittmann/bowlingscorer/BowlingLine.scala
@@ -1,8 +1,75 @@
 package com.mattwittmann.bowlingscorer
 
 object BowlingLine {
-  def score(line: String): Int = line.map {
-    case '-' => 0
-    case roll => roll.asDigit
-  }.sum
+  private[this] val TotalFrames = 10
+  private[this] val LastFrame = 9
+  type Score = Int
+  type BowlingLine = Seq[Score]
+  type Bonuses = (List[Int], List[Int])
+  sealed class Roll
+  object FirstRoll extends Roll
+  object SecondRoll extends Roll
+  object Over extends Roll
+  abstract class BonusRoll extends Roll
+  object LastBonusRoll extends BonusRoll
+  object FirstBonusRoll extends BonusRoll
+  private[this] val UnplayedFrames = Array.fill[Score](TotalFrames)(0)
+  private[this] val NoBonuses = (Nil, Nil)
+  case class State(currentFrame: Int = 0, roll: Roll = FirstRoll, bonuses: Bonuses = NoBonuses, frames: BowlingLine = UnplayedFrames)
+
+  def score(line: String): Score = {
+    line.foldLeft[State](State()) { (lastState, roll) =>
+      val currentFrame = lastState.currentFrame
+      val lastCurrentFrameScore = lastState.frames(currentFrame)
+      val (pinsKnockedDown, currentFrameScore) = roll match {
+        case 'X' => lastState.roll match {
+          case _: BonusRoll => (10, lastCurrentFrameScore + 10)
+          case _ => (10, 10)
+        }
+        case '/' => (10 - lastCurrentFrameScore, 10)
+        case '-' => (0, lastCurrentFrameScore)
+        case digit => {
+          val convertedDigit = digit.asDigit
+          (convertedDigit, lastCurrentFrameScore + convertedDigit)
+        }
+      }
+
+      val (nextFrame, nextRoll) =
+        if ('X' == roll && currentFrame == LastFrame && lastState.roll == FirstRoll)
+          (LastFrame, FirstBonusRoll)
+        else if ('/' == roll && currentFrame == LastFrame && lastState.roll == SecondRoll)
+          (LastFrame, LastBonusRoll)
+        else if (lastState.roll == FirstBonusRoll)
+          (LastFrame, LastBonusRoll)
+        else if (lastState.roll == LastBonusRoll)
+          (LastFrame, Over)
+        else if (lastState.roll == SecondRoll && currentFrame == LastFrame)
+          (LastFrame, Over)
+        else if (lastState.roll == SecondRoll || 'X' == roll)
+          (currentFrame + 1, FirstRoll)
+        else
+          (currentFrame, SecondRoll)
+
+      val nextBonuses =
+        if (roll == 'X')
+          (currentFrame :: lastState.bonuses._2, List(currentFrame))
+        else if (roll == '/')
+          (currentFrame :: lastState.bonuses._2, Nil)
+        else
+          (lastState.bonuses._2, Nil)
+
+      val nextFrames: BowlingLine = lastState.frames.zipWithIndex.map {
+        case (score, index) => {
+          if (index == currentFrame)
+            currentFrameScore
+          else if (lastState.bonuses._1.contains(index))
+            score + pinsKnockedDown
+          else
+            score
+        }
+      }
+
+      State(nextFrame, nextRoll, nextBonuses, nextFrames)
+    }.frames.sum
+  }
 }
