388a3c3998d34e709b1bcd2cba5a75e4a2582081
Matt Wittmann <matt@mattwittmann.com>
2017-04-23 20:15:51 -0500

    New test cases for input-validation errors

--- src/test/scala/com/mattwittmann/bowlingscorer/BowlingLineTest.scala
+++ src/test/scala/com/mattwittmann/bowlingscorer/BowlingLineTest.scala
@@ -10,27 +10,47 @@ class BowlingLineTest extends WordSpec {
   "A BowlingLine" when {
     "supplied a line of rolls XXXXXXXXXXXX" should {
       "yield a score of 300" in {
-        assert(BowlingLine.score("XXXXXXXXXXXX") === 300)
+        assert(BowlingLine.score("XXXXXXXXXXXX") === Right(300))
       }
     }
     "supplied a line of rolls 9-9-9-9-9-9-9-9-9-9-" should {
       "yield a score of 90" in {
-        assert(BowlingLine.score("9-9-9-9-9-9-9-9-9-9-") === 90)
+        assert(BowlingLine.score("9-9-9-9-9-9-9-9-9-9-") === Right(90))
       }
     }
     "supplied a line of rolls 5/5/5/5/5/5/5/5/5/5/5" should {
       "yield a score of 150" in {
-        assert(BowlingLine.score("5/5/5/5/5/5/5/5/5/5/5") === 150)
+        assert(BowlingLine.score("5/5/5/5/5/5/5/5/5/5/5") === Right(150))
       }
     }
     "supplied a line of rolls --------------------" should {
       "yield a score of 0" in {
-        assert(BowlingLine.score("--------------------") === 0)
+        assert(BowlingLine.score("--------------------") === Right(0))
       }
     }
     "supplied a line of rolls 44444444444444444444" should {
       "yield a score of 80" in {
-        assert(BowlingLine.score("44444444444444444444") === 80)
+        assert(BowlingLine.score("44444444444444444444") === Right(80))
+      }
+    }
+    "supplied a line of rolls 47444444444444444444" should {
+      "yield an error of TooManyPins" in {
+        assert(BowlingLine.score("47444444444444444444") === Left(BowlingLine.TooManyPins))
+      }
+    }
+    "supplied a line of rolls 444444444444444444444" should {
+      "yield an error of TooManyFrames" in {
+        assert(BowlingLine.score("444444444444444444444") === Left(BowlingLine.TooManyFrames))
+      }
+    }
+    "supplied a line of rolls 5/5/5/5/5/5/5/5/5/5/5X" should {
+      "yield an error of TooManyFrames" in {
+        assert(BowlingLine.score("5/5/5/5/5/5/5/5/5/5/5X") === Left(BowlingLine.TooManyFrames))
+      }
+    }
+    "supplied a line of rolls XXXXXXXXXXXXX" should {
+      "yield an error of TooManyFrames" in {
+        assert(BowlingLine.score("XXXXXXXXXXXXX") === Left(BowlingLine.TooManyFrames))
       }
     }
   }
